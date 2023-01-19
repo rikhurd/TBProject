ee5ed1f4a32bc1acaeab22a7f9374058bfcb7940
Riku Härkönen <riku.harkonen1@gmail.com>
2023-01-19 12:23:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TBS_Project
+
+Developed with Unreal Engine 5
---
+++ Source/TBS_Project/BoxBrush.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/Brush.h"
+#include "BoxBrush.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_PROJECT_API ABoxBrush : public ABrush
+{
+	GENERATED_BODY()
+	
+};
