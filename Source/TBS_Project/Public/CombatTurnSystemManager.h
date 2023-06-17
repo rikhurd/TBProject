8d05056bc66850a0108fa6ad1a637f16487fe921
Riku Härkönen <riku.harkonen1@gmail.com>
2023-06-17 18:21:36 +0300

    Create combat turn system
    This is used to handle combat system

---
+++ Source/TBS_Project/Public/CombatTurnSystemManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CombatTurnSystemManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_PROJECT_API UCombatTurnSystemManager : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+};
